8bd1bfbab1ea75c0fdf0511c637a57b20edc52a3
Archie To <toanhminh0412@gmail.com>
2025-11-07 07:49:43 -0800

    Build some queries

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,25 @@ from . import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+    path("products_under_ten_dollar", views.products_under_ten_dollar, name="products_under_ten_dollar"),
+    path("products_with_espresso_in_name", views.products_with_espresso_in_name, name="products_with_espresso_in_name"),
+    path("products_with_no_category", views.products_with_no_category, name="products_with_no_category"),
+    path("products_with_tea_category", views.products_with_tea_category, name="products_with_tea_category"),
+    path("products_with_caffeinated_tag", views.products_with_caffeinated_tag, name="products_with_caffeinated_tag"),
+    path("tags_with_at_least_one_product", views.tags_with_at_least_one_product, name="tags_with_at_least_one_product"),
+    path("milky_tag_related_products", views.milky_tag_related_products, name="milky_tag_related_products"),
+    path("categories_with_product_num", views.categories_with_product_num, name="categories_with_product_num"),
+    path("product_avg_price", views.product_avg_price, name="product_avg_price"),
+    path("tags_with_avg_price_gt_five", views.tags_with_avg_price_gt_five, name="tags_with_avg_price_gt_five"),
+    path("most_expensive_product_per_category", views.most_expensive_product_per_category, name="most_expensive_product_per_category"),
+    path("products_with_tag_count", views.products_with_tag_count, name="products_with_tag_count"),
+    path("products_with_tag_names", views.products_with_tag_names, name="products_with_tag_names"),
+    path("products_with_at_least_one_of_tags", views.products_with_at_least_one_of_tags, name="products_with_at_least_one_of_tags"),
+    path("products_with_none_of_tags", views.products_with_none_of_tags, name="products_with_none_of_tags"),
+    path("distinct_categories_over_products_span", views.distinct_categories_over_products_span, name="distinct_categories_over_products_span"),
+    path("case_insensitive_latte_search", views.case_insensitive_latte_search, name="case_insensitive_latte_search"),
+    path("products_above_average_price", views.products_above_average_price, name="products_above_average_price"),
+    path("products_in_categories_starting_with_c", views.products_in_categories_starting_with_c, name="products_in_categories_starting_with_c"),
+    path("discount_clearance_products", views.discount_clearance_products, name="discount_clearance_products"),
+    path("delete_unused_tags", views.delete_unused_tags, name="delete_unused_tags")
 ]
--- products/views.py
+++ products/views.py
@@ -1,10 +1,12 @@
 import re
 import time
 from django.views.generic import TemplateView
-from django.db.models import Count, Q
+from django.db.models import Count, Q, Avg, Max, Subquery, F
 from django.contrib.postgres.search import TrigramWordSimilarity
 from django.db.models.functions import Least
 from django.core.paginator import Paginator
+from django.core.serializers import serialize
+from django.http import JsonResponse
 from .models import Category, Tag, Product
 
 class IndexView(TemplateView):
@@ -95,7 +97,7 @@ class IndexView(TemplateView):
         end = time.perf_counter()
 
         # Context variables used for template
-        paginator =  Paginator(query, 25)
+        paginator = Paginator(query, 25)
         context["paginator"] = paginator
         context["product_page"] = paginator.get_page(page)
         context["categories"] = Category.objects.all()
@@ -107,3 +109,202 @@ class IndexView(TemplateView):
 
         context["query_execution_time"] = end - start
         return context
+
+def products_under_ten_dollar(request):
+    products = Product.objects.select_related("category").prefetch_related("tag")
+    products = products.filter(price__lt=10).order_by("price")
+    return JsonResponse({
+        "products": [product.serialize() for product in products]
+    })
+    
+def products_with_espresso_in_name(request):
+    products = Product.objects.select_related("category").prefetch_related("tag")
+    products = products.filter(name__icontains="espresso")
+    return JsonResponse({
+        "products": [product.serialize() for product in products]
+    })
+    
+def products_with_no_category(request):
+    products = Product.objects.select_related("category").prefetch_related("tag")
+    products = products.filter(category__isnull=True)
+    return JsonResponse({
+        "products": [product.serialize() for product in products]
+    })
+    
+def products_with_tea_category(request):
+    products = Product.objects.select_related("category").prefetch_related("tag")
+    products = products.filter(category__name__iexact="tea")
+    return JsonResponse({
+        "products": [product.serialize() for product in products]
+    })
+
+def products_with_caffeinated_tag(request):
+    products = Product.objects.select_related("category").prefetch_related("tag")
+    products = products.filter(tag__name="caffeinated")
+    return JsonResponse({
+        "products": [product.serialize() for product in products]
+    })
+
+def tags_with_at_least_one_product(request):
+    tags = Tag.objects.filter(product__isnull=False).distinct()
+    return JsonResponse({
+        "tags": [tag.serialize() for tag in tags]
+    })
+    
+def milky_tag_related_products(request):
+    (tag, _created) = Tag.objects.get_or_create(name="milky")
+    products = tag.product_set.all().prefetch_related("tag")
+    return JsonResponse({
+        "products": [product.serialize() for product in products]
+    })
+
+def categories_with_product_num(request):
+    categories = Category.objects.annotate(
+        product_num=Count('product')
+    ).order_by("-product_num")
+    return JsonResponse({
+        "categories": [{
+            "name": cat.name,
+            "product_num": cat.product_num    
+        } for cat in categories]
+    })
+
+def product_avg_price(request):
+    avg_price = Product.objects.aggregate(
+        Avg("price", default=0)
+    )["price__avg"]
+    return JsonResponse({
+        "Avg price": avg_price
+    })
+
+def tags_with_avg_price_gt_five(request):
+    tags = Tag.objects.annotate(
+        avg_product_price=Avg('product__price', default=0)
+    ).filter(avg_product_price__gt=5)
+    
+    return JsonResponse({
+        "tags": [
+            {
+                "name": tag.name,
+                "avg_price": float(tag.avg_product_price)
+            } for tag in tags
+        ]
+    })
+    
+def most_expensive_product_per_category(request):
+    categories = Category.objects.annotate(
+        max_price=Max('product__price', default=0)
+    ).values_list("id", "max_price")
+    
+    return JsonResponse({
+        "categories": list(categories)
+    })
+    
+def products_with_tag_count(request):
+    products = Product.objects.annotate(
+        tag_count=Count("tag", distinct=True)
+    ).order_by("-tag_count")
+    return JsonResponse({
+        "products": [
+            {
+                "name": product.name,
+                "tag_count": product.tag_count
+            } for product in products
+        ]
+    })
+    
+def products_with_tag_names(request):
+    tags = ["hot", "contains-dairy"]
+    products = Product.objects.annotate(
+        matched_tag_count=Count('tag', filter=Q(tag__name__in=tags), distinct=True)
+    ).filter(matched_tag_count=len(tags)).prefetch_related("tag")
+
+    return JsonResponse({
+        "products": [
+            {
+                "name": product.name,
+                "tags": [tag.name for tag in product.tag.all()]
+            } for product in products
+        ]
+    })
+
+def products_with_at_least_one_of_tags(request):
+    tags = ["cold", "something"]
+    products = Product.objects.filter(tag__name__in=tags).prefetch_related("tag").distinct()
+    
+    return JsonResponse({
+        "products": [
+            {
+                "name": product.name,
+                "tags": [tag.name for tag in product.tag.all()]
+            } for product in products
+        ]
+    })
+    
+def products_with_none_of_tags(request):
+    tags = ["cold", "hot"]
+    # products = Product.objects.filter(
+    #     ~Q(tag__name__in=tags)
+    # ).prefetch_related("tag").distinct()
+    
+    products = Product.objects.exclude(tag__name__in=tags).prefetch_related("tag").distinct()
+    
+    return JsonResponse({
+        "products": [
+            {
+                "name": product.name,
+                "tags": [tag.name for tag in product.tag.all()]
+            } for product in products
+        ]
+    })
+    
+def distinct_categories_over_products_span(request):
+    tags = Tag.objects.annotate(
+        cat_num=Count("product__category__name", distinct=True)
+    ).distinct()
+    
+    return JsonResponse({
+        "tags": [
+            {
+                "name": tag.name,
+                "cat_num": tag.cat_num
+            } for tag in tags
+        ]
+    })
+
+def case_insensitive_latte_search(request):
+    products = Product.objects.select_related("category").prefetch_related("tag")
+    products = products.filter(Q(name__icontains="latte") | Q(description__icontains="latte"))
+    return JsonResponse({
+        "products": [product.serialize() for product in products] if products else []
+    })
+
+def products_above_average_price(request):
+    avg_price = Product.objects.aggregate(avg_price=Avg("price", default=0))["avg_price"]
+    products = Product.objects.filter(price__gt=avg_price)
+    
+    return JsonResponse({
+        "products": [product.serialize() for product in products] if products else []
+    })
+
+def products_in_categories_starting_with_c(request):
+    products = Product.objects.filter(category__name__istartswith="c")
+    return JsonResponse({
+        "products": [product.serialize() for product in products] if products else []
+    })
+
+def discount_clearance_products(request):
+    products = Product.objects.filter(category__name="Coffee").update(price=F("price") * 0.9)
+
+    updated_count = 0
+    return JsonResponse({
+        "updated_count": updated_count,
+        "message": f"Applied 10% discount to {updated_count} products in Clearance category"
+    })
+
+def delete_unused_tags(request):
+    deleted_count = 0
+    return JsonResponse({
+        "deleted_count": deleted_count,
+        "message": f"Deleted {deleted_count} unused tags"
+    })
